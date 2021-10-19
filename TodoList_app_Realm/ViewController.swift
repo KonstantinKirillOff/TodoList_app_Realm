141e667743dec6d30f727927a4a9f9d5c64e7796
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-10-19 09:50:25 +0300

    Initial Commit

---
+++ TodoList_app_Realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList_app_Realm
+//
+//  Created by Константин Кириллов on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
